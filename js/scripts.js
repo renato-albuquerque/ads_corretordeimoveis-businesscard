91f13b08616c465deddecc8bdbca3fc3887ad77a
Renato Albuquerque <renatomalb83@gmail.com>
2023-08-24 10:17:34 -0300

    updated URL function share, js file

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function share(){
 		navigator.share({
 			title: 'ADS Corretor de Imóveis - Cartão de Visita Digital',
 			text: 'Aluguel, Compra, Venda e Atividades Afins.',
-			url: 'http://www.adscorretordeimoveis.com.br/'  /* CHANGE URL */
+			url: 'https://adscorretordeimoveis-cartaodevisita.vercel.app/'
 		})
 		.then(() => console.log('Successful share'))
 		.catch((error) => console.log('Error sharing', error));
